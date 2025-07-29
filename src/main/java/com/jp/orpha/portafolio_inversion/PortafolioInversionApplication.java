bd9ffcecb25f053cc6babd6bbba9b6b2e149798f
jporpha <jporpha@myhotel.cl>
2025-07-29 10:25:07 -0400

    Primer commit - estructura base del proyecto

---
+++ src/main/java/com/jp/orpha/portafolio_inversion/PortafolioInversionApplication.java
@@ -0,0 +1,13 @@
+package com.jp.orpha.portafolio_inversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioInversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioInversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/orpha/portafolio_inversion/PortafolioInversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.orpha.portafolio_inversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioInversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
